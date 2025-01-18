27ffd92c43140ff97f8f0a2f7a590e2052df19c0
Jeywin <j3ywin@gmail.com>
2025-01-18 15:08:58 +0300

    Create header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './header.css';
+
+const Header = () => {
+  var navbar = (
+    <div className="navbar">
+      <ul className='nav-left'>
+        <li><a href="#home">Главная</a></li>
+        <li><a href="#catalog">Каталог</a></li>
+        <li><a href="#about">О нас</a></li>
+      </ul>
+      <ul className='nav-right'>
+        <li><a href="#cart">Корзина</a></li>
+        <li><a href="#profile">Профиль</a></li>
+      </ul>
+    </div>
+  );
+
+  return navbar;
+}
+
+export default Header;
