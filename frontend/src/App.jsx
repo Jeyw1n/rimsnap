5a994ca190f9004205e6b7a1b653eb74374072f6
Jeywin <j3ywin@gmail.com>
2025-01-18 15:07:44 +0300

    Add header to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 import Auth from './components/Auth/Auth'
 import Profile from './components/Profile/Profile'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <Auth />
       <Profile />
     </>
