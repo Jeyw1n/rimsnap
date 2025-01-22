e60e9b8f2b80f6977f1d7c4b327f622b16f5055d
Jeywin <j3ywin@gmail.com>
2025-01-22 17:24:29 +0300

    Improve the header component

--- frontend/src/components/Main/Title.jsx
+++ frontend/src/components/Main/Title.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import './title.css'
+import "./animation.css";
 import carAnimation from "./car_animation.webp";
 
 const Title = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setVisible(true); // Устанавливаем видимость в true после загрузки страницы
+    };
+
+    window.addEventListener('load', handleLoad); // Добавляем обработчик события load
+
+    return () => {
+      window.removeEventListener('load', handleLoad); // Удаляем обработчик при размонтировании
+    };
+  }, []);
+
   return (
     <div className="title-block">
       <h1>RIMSNAP</h1>
-      <img src={carAnimation} alt="Animated car" />
+      <img 
+        src={carAnimation} 
+        alt="Animated car" 
+        className={`fade-in-left ${visible ? 'visible' : ''}`} 
+      />
     </div>
   );
 }
