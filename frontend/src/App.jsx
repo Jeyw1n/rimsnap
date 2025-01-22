23d3114407d4f2a9ae3d97ab5aa75bcd68596377
Jeywin <j3ywin@gmail.com>
2025-01-22 17:22:45 +0300

    Connect MenuBlocks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-
 import Auth from './components/Auth/Auth'
 import Profile from './components/Profile/Profile'
 import Header from './components/Header/Header'
 import Title from './components/Main/Title'
+import MenuBlocks from './components/Main/MenuBlocks'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
       <Title />
+      <MenuBlocks />
       <Auth />
       <Profile />
     </>
