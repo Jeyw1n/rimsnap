026520b5e198ce8ca864dfbe85c321ed52cd5e49
Jeywin <j3ywin@gmail.com>
2025-01-31 23:20:13 +0300

    Create footer component

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+    return(
+        <footer className="mid-block">
+            <div>
+                <p>© RimSnap 2025</p>
+            </div>
+            <div>
+                <p>+71234567890</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
