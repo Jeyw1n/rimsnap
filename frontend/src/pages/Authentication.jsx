569757f1c779a456774d2b2c26451fd44cfc878e
Jeywin <j3ywin@gmail.com>
2025-01-31 23:20:37 +0300

    Add auth page component

---
+++ frontend/src/pages/Authentication.jsx
@@ -0,0 +1,13 @@
+import Auth from "../components/Auth/Auth";
+import Header from "../components/Header/Header";
+
+const Authentication = () => {
+    return(
+        <>
+            <Header />
+            <Auth />
+        </>
+    );
+}
+
+export default Authentication;
