890b8b9862494d0b4cba2478f5c7b8c929fdca0a
Jeywin <j3ywin@gmail.com>
2025-01-31 23:38:58 +0300

    Clean imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,6 @@ import {
   Route, Routes
 } from 'react-router';
 
-import Auth from './components/Auth/Auth'
-import Profile from './components/Profile/Profile'
-import Header from './components/Header/Header'
-import Title from './components/Main/Title'
-import MenuBlocks from './components/Main/MenuBlocks'
-import Content from './components/Main/Content'
-import Products from './components/Shop/Products'
-
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import Authentication from './pages/Authentication';
