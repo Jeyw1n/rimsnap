93a2ab46bf94690bbbc9418912b0f2acc74b86a0
Jeywin <j3ywin@gmail.com>
2025-02-16 15:41:09 +0300

    Set url pattern for the LastProductsView

--- backend/rimsnap/products/urls.py
+++ backend/rimsnap/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductModelView
+from .views import ProductModelView, LastProductsView
 
 urlpatterns = [
     path('products/', ProductModelView.as_view(), name='products'),
+    path('last-products/', LastProductsView.as_view(), name='last_products')
 ]
