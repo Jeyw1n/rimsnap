d2cbc8fad692e1006d0d9a4955193954d63c56ed
Jeywin <j3ywin@gmail.com>
2025-02-16 15:40:38 +0300

    Create LastProductsView

--- backend/rimsnap/products/views.py
+++ backend/rimsnap/products/views.py
@@ -7,3 +7,12 @@ from .serializers import ProductSerializer
 class ProductModelView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class LastProductsView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        count = self.request.query_params.get('count', 5)  # Получаем параметр count из запроса, по умолчанию 5
+        count = int(count)  # Преобразуем в целое число
+        # return Product.objects.all().order_by('-popularity')[:count]  # Получаем самые популярные
+        return Product.objects.all().order_by()[:count]
