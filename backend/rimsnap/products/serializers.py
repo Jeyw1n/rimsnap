5b3d5d35d3a80fce0894d626007db83d7cafb99f
UniqueUser12sasa3 <uniqueuser12sasa3@example.com>
2025-02-26 11:28:48 +0300

    New serializers

--- backend/rimsnap/products/serializers.py
+++ backend/rimsnap/products/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, ProductImage
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['image']
 
 class ProductSerializer(serializers.ModelSerializer):
+    # Поле для одной картинки (используется в списке товаров)
+    image = serializers.SerializerMethodField()
+    # Поле для всех картинок (используется в детальном представлении)
+    images = ProductImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'tags', 'image', 'price']
+        fields = ['id', 'name', 'description', 'tags', 'price', 'image', 'images']
+
+    # Метод для получения одной картинки (первой из списка)
+    def get_image(self, obj):
+        first_image = obj.images.first()  # Получаем первую картинку
+        if first_image:
+            # Получаем полный URL для первой картинки
+            request = self.context.get('request')
+            return request.build_absolute_uri(first_image.image.url)
+        return None
