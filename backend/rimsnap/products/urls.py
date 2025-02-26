c429fa394f0aa6985b3f691f62a7c3c0e4af6efa
UniqueUser12sasa3 <uniqueuser12sasa3@example.com>
2025-02-26 11:29:19 +0300

    Add a url for one product

--- backend/rimsnap/products/urls.py
+++ backend/rimsnap/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductModelView, LastProductsView
+from .views import ProductModelView, LastProductsView, ProductDetailView
 
 urlpatterns = [
     path('products/', ProductModelView.as_view(), name='products'),
+    path('products/<int:id>/', ProductDetailView.as_view(), name='product_detail'),  # Детали товара
     path('last-products/', LastProductsView.as_view(), name='last_products')
 ]
--- backend/rimsnap/products/views.py
+++ backend/rimsnap/products/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-
 class ProductModelView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -14,5 +13,10 @@ class LastProductsView(generics.ListAPIView):
     def get_queryset(self):
         count = self.request.query_params.get('count', 5)  # Получаем параметр count из запроса, по умолчанию 5
         count = int(count)  # Преобразуем в целое число
-        # return Product.objects.all().order_by('-popularity')[:count]  # Получаем самые популярные
         return Product.objects.all().order_by()[:count]
+
+# представление для получения одного товара по id (возвращает все картинки)
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'  # Поле для поиска товара (по умолчанию 'pk', но можно указать 'id')
