c3b485e941641507c8a7ee0fcdb7493233187d6b
UniqueUser12sasa3 <uniqueuser12sasa3@example.com>
2025-02-26 11:28:03 +0300

    Adapt the admin panel, add the addition of images on the product page

--- backend/rimsnap/products/admin.py
+++ backend/rimsnap/products/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductImage
 
-admin.site.register(Product)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1  # Количество пустых форм для добавления новых картинок
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    inlines = [ProductImageInline]  # Добавляем вложенное редактирование картинок
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['product', 'image_preview']
